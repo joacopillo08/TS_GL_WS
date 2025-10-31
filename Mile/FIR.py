d9b9be4fe19a653c058bcb7491fae09d8a490aac
joacomillo12 <joacomillo12@gmail.com>
2025-10-31 00:11:09 -0300

    FILTROS FIR

--- Mile/FIR.py
+++ Mile/FIR.py
@@ -1,30 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 30 19:19:28 2025
 
+@author: JGL
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 import scipy.io as sio
 import matplotlib.patches as patches
+from scipy.signal import firwin2, freqz
 
-#filtro normalizado -> todas las singularidades en el circulo unitario?
-#--- Plantilla de diseño ---
-
-fs = 1000
+fs = 2000
 wp = [0.8, 35] #freq de corte/paso (rad/s)
-ws = [0.1, 40] #freq de stop/detenida (rad/s)
+ws = [0.1, 35.7] #freq de stop/detenida (rad/s)
 
 #si alpha_p es =3 -> max atenuacion, butter
 
 alpha_p = 1/2 #atenuacion de corte/paso, alfa_max, perdida en banda de paso 
 alpha_s = 40/2 #atenuacion de stop/detenida, alfa_min, minima atenuacion requerida en banda de paso 
 
-f_aprox = 'cauer'
-mi_sos_cauer = signal.iirdesign(wp = wp, ws = ws, gpass = alpha_p, gstop = alpha_s, analog = False, ftype = f_aprox, output ='sos', fs=fs) #devuelve dos listas de coeficientes, b para P y a para Q
+
+# frecuencias= np.sort(np.concatenate(0, wp,ws, fs/2))
+frecuencias= np.sort(np.concatenate(((0,fs/2), wp,ws)))
+
+deseado = [0,0,1,1,0,0]
+cant_coef = 2000
+retardo = (cant_coef-1)//2
+
+
+fir_win = firwin2(numtaps = cant_coef, freq = frecuencias, window = "boxcar",nfreqs = cant_coef**2-1, gain = deseado, fs = fs)
 
 # %%
-mi_sos = mi_sos_cauer
 
 # --- Respuesta en frecuencia ---
-w, h= signal.freqz_sos(mi_sos, worN = np.logspace(-2, 1.9, 1000), fs = fs) #calcula rta en frq del filtro, devuelve w y vector de salida (h es numero complejo)
+w, h= freqz(fir_win, worN = np.logspace(-2, 2, 1000), fs = fs) #calcula rta en frq del filtro, devuelve w y vector de salida (h es numero complejo)
 
 # --- Cálculo de fase y retardo de grupo ---
 
@@ -35,22 +45,22 @@ gd = -np.diff(fase) / np.diff(w_rad) #retardo de grupo [rad/rad]
 
 # --- Polos y ceros ---
 
-z, p, k = signal.sos2zpk(mi_sos) #ubicacion de polos y ceros, z=ubicacion de ceros(=0), p=ubicacion polos, k
+z, p, k = signal.sos2zpk(signal.tf2sos(fir_win,a= 1)) #ubicacion de polos y ceros, z=ubicacion de ceros(=0), p=ubicacion polos, k
 
 # --- Gráficas ---
 #plt.figure(figsize=(12,10))
 
 # Magnitud
 plt.subplot(3,1,1)
-plt.plot(w, 20*np.log10(abs(h)), label = f_aprox)
+plt.plot(w, 20*np.log10(abs(h)))
 plt.title('Respuesta en Magnitud')
 plt.xlabel('Pulsación angular [r/s]')
-plt.ylabel('|H(jω)| [dB]')
+plt.ylabel('|H(z)| [dB]')
 plt.grid(True, which='both', ls=':')
 
 # Fase
 plt.subplot(3,1,2)
-plt.plot(w, fase, label = f_aprox)
+plt.plot(w, fase)
 plt.title('Fase')
 plt.xlabel('Pulsación angular [r/s]')
 plt.ylabel('Fase [°]')
@@ -58,52 +68,36 @@ plt.grid(True, which='both', ls=':')
 
 # Retardo de grupo
 plt.subplot(3,1,3)
-plt.plot(w[:-1], gd, label = f_aprox)
+plt.plot(w[:-1], gd)
 plt.title('Retardo de Grupo ')
 plt.xlabel('Pulsación angular [r/s]')
 plt.ylabel('τg [# muestras]')
 plt.grid(True, which='both', ls=':')
 
-# # Diagrama de polos y ceros
-# plt.subplot(2,2,4)
+
+# plt.figure(figsize=(10,10))
 # plt.plot(np.real(p), np.imag(p), 'x', markersize=10, label=f'{f_aprox} Polos')
+# axes_hdl = plt.gca()
+
 # if len(z) > 0:
 #     plt.plot(np.real(z), np.imag(z), 'o', markersize=10, fillstyle='none', label=f'{f_aprox} Ceros')
+
+# # Ejes y círculo unitario
 # plt.axhline(0, color='k', lw=0.5)
 # plt.axvline(0, color='k', lw=0.5)
+# unit_circle = patches.Circle((0, 0), radius=1, fill=False, color='gray', ls='dotted', lw=2)
+# axes_hdl.add_patch(unit_circle)
+
+# # Ajustes visuales
+# plt.axis([-1.1, 1.1, -1.1, 1.1])
 # plt.title('Diagrama de Polos y Ceros (plano z)')
-# plt.xlabel('σ [rad/s]')
-# plt.ylabel('jω [rad/s]')
+# plt.xlabel(r'$\Re(z)$')
+# plt.ylabel(r'$\Im(z)$')
 # plt.legend()
 # plt.grid(True)
-
 # plt.tight_layout()
 # plt.show()
 
-# %% Gráfico adicional del plano Z con círculo unitario
-
-plt.figure(figsize=(10,10))
-plt.plot(np.real(p), np.imag(p), 'x', markersize=10, label=f'{f_aprox} Polos')
-axes_hdl = plt.gca()
-
-if len(z) > 0:
-    plt.plot(np.real(z), np.imag(z), 'o', markersize=10, fillstyle='none', label=f'{f_aprox} Ceros')
-
-# Ejes y círculo unitario
-plt.axhline(0, color='k', lw=0.5)
-plt.axvline(0, color='k', lw=0.5)
-unit_circle = patches.Circle((0, 0), radius=1, fill=False, color='gray', ls='dotted', lw=2)
-axes_hdl.add_patch(unit_circle)
-
-# Ajustes visuales
-plt.axis([-1.1, 1.1, -1.1, 1.1])
-plt.title('Diagrama de Polos y Ceros (plano z)')
-plt.xlabel(r'$\Re(z)$')
-plt.ylabel(r'$\Im(z)$')
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
-plt.show()
 #%%
 
 ##################
@@ -122,12 +116,12 @@ mat_struct = sio.loadmat('./ECG_TP4.mat')
 ecg_one_lead = mat_struct['ecg_lead'].flatten()
 N = len(ecg_one_lead)
 
-ecg_filt_cauer = signal.sosfiltfilt(mi_sos_cauer, ecg_one_lead)
+ecg_filt_win = signal.lfilter(b = fir_win, a = 1, x = ecg_one_lead)
 
 plt.figure()
 
 plt.plot(ecg_one_lead, label = 'ecg raw')
-plt.plot(ecg_filt_cauer, label = 'cauer')
+plt.plot(ecg_filt_win, label = 'cauer')
 
 plt.legend()
 
@@ -137,7 +131,7 @@ t = np.arange(N) / fs_ecg  # vector de tiempo en segundos
 # plt.plot(t[4000:5500], ecg_one_lead[4000:5500], label='ECG crudo')
 # plt.plot(t[4000:5500], ecg_filt_cauer[4000:5500], label='ECG filtrado (Cauer)')
 plt.plot(ecg_one_lead[80750:89000], label='ECG crudo')
-plt.plot(ecg_filt_cauer[80750:89000], label='ECG filtrado (Cauer)')
+plt.plot(ecg_filt_win[80750:89000], label='ECG filtrado (Cauer)')
 # plt.xlabel('Tiempo [s]')
 plt.ylabel('Amplitud')
 plt.title('ECG con y sin filtrado')
@@ -164,8 +158,8 @@ for ii in regs_interes:
    
     plt.figure()
     plt.plot(zoom_region, ecg_one_lead[zoom_region], label='ECG', linewidth=2)
-    plt.plot(zoom_region, ecg_filt_cauer[zoom_region], label='Butterworth')
-    #plt.plot(zoom_region, ECG_f_win[zoom_region + demora], label='FIR Window')
+    # plt.plot(zoom_region, ecg_filt_win[zoom_region], label='Butterworth')
+    plt.plot(zoom_region, ecg_filt_win[zoom_region + retardo], label='FIR Window')
    
     plt.title('ECG sin ruido desde ' + str(ii[0]) + ' to ' + str(ii[1]) )
     plt.ylabel('Adimensional')
@@ -194,8 +188,8 @@ for ii in regs_interes:
    
     plt.figure()
     plt.plot(zoom_region, ecg_one_lead[zoom_region], label='ECG', linewidth=2)
-    plt.plot(zoom_region, ecg_filt_cauer[zoom_region], label='Butterworth')
-    # plt.plot(zoom_region, ECG_f_win[zoom_region + demora], label='FIR Window')
+    # plt.plot(zoom_region, ecg_filt_win[zoom_region], label='Butterworth')
+    plt.plot(zoom_region, ecg_filt_win[zoom_region + retardo], label='FIR Window')
    
     plt.title('ECG con ruido desde ' + str(ii[0]) + ' to ' + str(ii[1]) )
     plt.ylabel('Adimensional')
