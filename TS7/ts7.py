09cf392224a7897a27a7fe80805107d6ec87952f
Mile Waichman <milewaichman@scigliano.net>
2025-11-10 11:10:40 -0300

    paso lo que hicimos en la rama mile al master

--- TS7/ts7.py
+++ TS7/ts7.py
@@ -10,13 +10,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-
-
-
 fs = 1000
 
 
-def resp_en_w(b,a,fs = 1000):
+def resp_en_w(b, a, nombre = 'Filtro', fs = 1000):
     w, h= signal.freqz(b = b, a = a, worN = 1024, fs = fs) #calcula rta en frq del filtro, devuelve w y vector de salida (h es numero complejo)
 
     fase = np.unwrap(np.angle(h)) #unwrap hace grafico continuo
@@ -26,15 +23,12 @@ def resp_en_w(b,a,fs = 1000):
 
     z, p, k =  signal.tf2zpk(b, a) #ubicacion de polos y ceros, z=ubicacion de ceros(=0), p=ubicacion polos, k
     
-        # --- Gráficas ---
-    #plt.figure(figsize=(12,10))
-    
     # Magnitud
     plt.figure()
     plt.subplot(2,2,1)
-    plt.plot(w, 20*np.log10(abs(h)))
-    plt.title('Respuesta en Magnitud')
-    plt.xlabel('Pulsación angular [r/s]')
+    plt.plot(w, 20*np.log10(abs(h)+ 1e-12))
+    plt.title(f'{nombre}: Respuesta en Magnitud')
+    plt.xlabel('Pulsación angular [Hz]')
     plt.ylabel('|H(jω)| [dB]')
     plt.grid(True, which='both', ls=':')
     
@@ -42,18 +36,21 @@ def resp_en_w(b,a,fs = 1000):
     plt.subplot(2,2,2)
     plt.plot(w, fase)
     plt.title('Fase')
-    plt.xlabel('Pulsación angular [r/s]')
+    plt.xlabel(f'{nombre}:Pulsación angular [Hz]')
     plt.ylabel('Fase [rad]')
     plt.grid(True, which='both', ls=':')
     
     # Retardo de grupo
     plt.subplot(2,2,3)
     plt.plot(w[:-1], gd)
-    plt.title('Retardo de Grupo ')
+    plt.title(f'{nombre}: Retardo de Grupo ')
     plt.xlabel('Pulsación angular [r/s]')
     plt.ylabel('τg [# muestras]')
     plt.grid(True, which='both', ls=':')
-    plt.legend()
+
+    plt.tight_layout()
+    plt.show
+        
 
 b1 = ([1,1,1,1])
 a1 = ([1])
@@ -67,7 +64,7 @@ a3 = ([1])
 b4 = ([1,0,-1])
 a4 = ([1])
 
-funcionA = resp_en_w(b = b1, a = a1)
-funcionB = resp_en_w(b = b2, a = a2)
-funcionC = resp_en_w(b = b3, a = a3)
-funcionD = resp_en_w(b = b4, a = a4)
+funcionA = resp_en_w(b = b1, a = a1, nombre = 'Filtro a')
+funcionB = resp_en_w(b = b2, a = a2, nombre = 'Filtro b')
+funcionC = resp_en_w(b = b3, a = a3, nombre = 'Filtro c')
+funcionD = resp_en_w(b = b4, a = a4, nombre = 'Filtro d')
